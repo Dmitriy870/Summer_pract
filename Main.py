a99463660ade94b0dad7a7d942e3c259d713027b
Dmitriy Pilevich <pilevichdima60@gmail.com>
2024-08-22 14:25:28 +0300

    Info about 3 branch

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
+
 print("Hello World!")
 print("Hello World!")
 print("hello from linux")
+name = "Boris"
+age= 69.1
+print("Hello, %s, i am %d" % (name,age)
+
+print("Hello from 3 branch named fix")
