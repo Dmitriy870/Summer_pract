87e6430e981219aa4a47a78d650ef181d60b7f00
Dmitriy Pilevich <pilevichdima60@gmail.com>
2024-08-23 10:12:06 +0300

    Syntaxsis fix

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
-
 print("Hello World!")
 print("Hello World!")
 print("hello from linux")
 name = "Boris"
-age= 69.1
-print("Hello, %s, i am %d" % (name,age)
+age = 69.1
+print("Hello, %s, i am %d" % (name, age))
 
 print("Hello from 3 branch named fix")
 print("want to add something new!")
