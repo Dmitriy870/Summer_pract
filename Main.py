92216bc8d476667c5ef93d23d5e180be4bf898d7
Dmitriy Pilevich <pilevichdima60@gmail.com>
2024-08-23 11:07:10 +0300

    Import randint

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from random import randint
 print("Hello World!")
 print("Hello World!")
 print("hello from linux")
@@ -8,3 +9,5 @@ print("Hello, %s, i am %d" % (name, age))
 print("Hello from 3 branch named fix")
 print("want to add something new!")
 print("want to add something new!")
+r_int = randint(1, 100)
+print(f"I have random number for you : {r_int}")
